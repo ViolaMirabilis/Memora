9d5478c96b0966b0af7e479db5dabd2c838bed20
Mirabilis <zajaclukasz64@gmail.com>
2026-01-04 21:59:28 +0100

    Add project files.

---
+++ Memora/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Memora
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
