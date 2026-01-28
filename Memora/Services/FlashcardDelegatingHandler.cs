6705374220dac2d6ef8ebf90a1a2717579456964
Mirabilis <zajaclukasz64@gmail.com>
2026-01-28 17:04:15 +0100

    Changed the name of the Delegating Handler.

--- Memora/Services/FlashcardDelegatingHandler.cs
+++ Memora/Services/FlashcardDelegatingHandler.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Memora.Interfaces;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
-namespace Memora.Services
+namespace Memora.Services;
+
+class JwtTokenHandler : DelegatingHandler
 {
-    class FlashcardDelegatingHandler
+
+    private readonly ITokenStore _tokenStore;
+
+    public JwtTokenHandler(ITokenStore tokenStore)
     {
+        _tokenStore = tokenStore;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            // Adds the JWT token to the request headers
+            if (!string.IsNullOrEmpty(_tokenStore.Token))
+            {
+                request.Headers.Authorization =
+                    new AuthenticationHeaderValue("Bearer", _tokenStore.Token);
+            }
+
+            return base.SendAsync(request, cancellationToken).Result;
+            
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.ToString());
+        }
+        
     }
 }
