2a34cc6875c5d797d3ca62d82c07dea96155c937
Mirabilis <zajaclukasz64@gmail.com>
2026-02-02 11:46:52 +0100

    Implemented a blueprint of a ServiceSession class, which holds information related to the current session, including currently used/requested flashcard folder, set, individual flashcard, etc.

---
+++ Memora/Services/SessionService.cs
@@ -0,0 +1,13 @@
+﻿using Memora.Model;
+namespace Memora.Services;
+
+/// <summary>
+/// WIP. The service will store information related to the current session, including currently used/requested flashcard folders, sets, progress, etc.
+/// </summary>
+public class SessionService
+{
+    // all can be nullable, because everything is optional
+    public Flashcard? flashcard { get; set; }
+    public FlashcardSet? flashcardSet { get; set; }
+    public FlashcardFolder? flashcardFolder { get; set; }
+}
