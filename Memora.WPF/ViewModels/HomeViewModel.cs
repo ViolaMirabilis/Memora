10aed849bf77ee365901a8f608a05c2cb0d36693
Mirabilis <zajaclukasz64@gmail.com>
2026-04-15 15:29:50 +0200

    Implementing a work in progress HomeView and ViewModel

--- Memora.WPF/ViewModels/HomeViewModel.cs
+++ Memora.WPF/ViewModels/HomeViewModel.cs
@@ -1,7 +1,38 @@
 ﻿using Memora.Core;
+using Memora.Interfaces;
+using Memora.Model;
+using System.Collections.ObjectModel;
 
 namespace Memora.ViewModels;
 
 public class HomeViewModel : ViewModel
 {
+
+    public ObservableCollection<FlashcardSet> FlashcardSetsView { get; }
+
+    private INavigationService _navigation;
+    public INavigationService Navigation
+    {
+        get => _navigation;
+        set
+        {
+            _navigation = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public HomeViewModel(INavigationService navigation)
+    {
+        _navigation = navigation;
+        FlashcardSetsView = new ObservableCollection<FlashcardSet>{
+            new FlashcardSet { Name = "English" },
+            new FlashcardSet { Name = "Polish" },
+            new FlashcardSet { Name = "Comp SCI" },
+            new FlashcardSet { Name = "Maths" },
+            new FlashcardSet { Name = "Reverse engineering" },
+            new FlashcardSet { Name = "Algebra" },
+            new FlashcardSet { Name = "Embedded systems" },
+            new FlashcardSet { Name = "Databases" },
+            new FlashcardSet { Name = "Programming" } };
+    }
 }
